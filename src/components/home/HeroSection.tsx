fac26d18db556a3cfbc39c9278d3b46d9e7ebfcc
UJWAL SINGH <ujwalsingh164@gmail.com>
2025-04-13 16:54:06 +0530

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
       {/* Hero Background with Overlay */}
       <div className="absolute inset-0 bg-cover bg-center" 
         style={{
-          backgroundImage: "url('https://www.k1travels.com/wp-content/uploads/2023/04/1440x810_cmsv2_6187d843-7037-5073-92f1-c3a894a93ba2-5368390.jpg')",
+          backgroundImage: "url('https://htoindia.com/wp-content/uploads/2019/06/HTO-banner-top-30-places.jpg')",
           backgroundPosition: "center",
         }}>
         <div className="absolute inset-0 bg-black/50"></div>
