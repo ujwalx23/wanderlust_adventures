b62c5fb6f13a606512850a9418e2fe94d14d2507
UJWAL SINGH <ujwalsingh164@gmail.com>
2025-04-13 21:46:10 +0530

    Update SearchBox.tsx

--- src/components/shared/SearchBox.tsx
+++ src/components/shared/SearchBox.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { destinations, faqs } from '@/constants/destinations';
 
 // Define search result types
-type SearchResultType = 'destination' | 'faq' | 'blog' | 'gallery' | 'music' | 'movie' | 'book';
+type SearchResultType = 'destination' | 'faq' | 'blog' | 'gallery' | 'music' | 'book';
 
 interface SearchResult {
   id: string;
